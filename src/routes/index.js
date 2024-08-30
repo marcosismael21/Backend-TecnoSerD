d77b3e77311f49a6a4c7537a2e041770d675df60
cernajr <jorcer26@gmail.com>
2024-08-30 10:18:17 -0600

    Se creo el modulo de Publicidad_Regalia, hice las funciones crear, eliminar, editar, ver y tambien elimine el archivo de transacciones varias

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const router = Router();
 
 //variables que heredan las routes
 const authRoutes = require('./usuario.routes')
+const publicidadRegaliaRoutes = require('./publicidadRegalia.routes')
 
 //establecer las rutas web
 router.use('/api/v1/usuario', authRoutes);
+router.use('/api/v1/pubreg', publicidadRegaliaRoutes)
 
 module.exports = router;
