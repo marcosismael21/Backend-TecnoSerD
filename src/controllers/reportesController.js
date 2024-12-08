da1afcf01c1bc638f2a88112405b1a81bc18953f
marcosismael21 <marcosismaelrodriguez949@gmail.com>
2024-12-07 20:51:51 -0600

    se crearon nuevos metodos para reporteria

--- src/controllers/reportesController.js
+++ src/controllers/reportesController.js
@@ -40,9 +40,53 @@ const getAllAsignacionEsperaByCiudadServicio = async (req, res, next) => {
     }
 }
 
+const getAllAsignacionFinalizado = async (req, res, next) => {
+    try {
+        const reportes = await reportesServices.getAllAsignacionFinalizado()
+        return res.status(200).json(reportes)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getAllAsignacionEsperaByCiudadFinalizada = async (req, res, next) => {
+    const idCiudad = req.params.idCiudad
+    try {
+        const reportes = await reportesServices.getAllAsignacionEsperaByCiudadFinalizada(idCiudad)
+        return res.status(200).json(reportes)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getAllAsignacionEsperaByServicioFinalizada = async (req, res, next) => {
+    const idServicio = req.params.idServicio
+    try {
+        const reportes = await reportesServices.getAllAsignacionEsperaByServicioFinalizada(idServicio)
+        return res.status(200).json(reportes)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getAllAsignacionEsperaByCiudadServicioFinalizada = async (req, res, next) => {
+    const idCiudad = req.params.idCiudad
+    const idServicio = req.params.idServicio
+    try {
+        const reportes = await reportesServices.getAllAsignacionEsperaByCiudadServicioFinalizada(idCiudad, idServicio)
+        return res.status(200).json(reportes)
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports = {
     getAllAsignacionEspera,
     getAllAsignacionEsperaByCiudad,
     getAllAsignacionEsperaByServicio,
     getAllAsignacionEsperaByCiudadServicio,
+    getAllAsignacionFinalizado,
+    getAllAsignacionEsperaByCiudadFinalizada,
+    getAllAsignacionEsperaByServicioFinalizada,
+    getAllAsignacionEsperaByCiudadServicioFinalizada,
 }
--- src/services/reportesServices.js
+++ src/services/reportesServices.js
@@ -36,9 +36,49 @@ const getAllAsignacionEsperaByCiudadServicio = async (idCiudad, idServicio) => {
     }
 }
 
+const getAllAsignacionFinalizado = async () => {
+    try {
+        const reportes = await reportesRepository.getAllAsignacionFinalizado()
+        return (reportes) ? reportes : []
+    } catch (error) {
+        throw error
+    }
+}
+
+const getAllAsignacionEsperaByCiudadFinalizada = async (idCiudad) => {
+    try {
+        const reportes = await reportesRepository.getAllAsignacionEsperaByCiudadFinalizada(idCiudad)
+        return (reportes) ? reportes : []
+    } catch (error) {
+        throw error
+    }
+}
+
+const getAllAsignacionEsperaByServicioFinalizada = async (idServicio) => {
+    try {
+        const reportes = await reportesRepository.getAllAsignacionEsperaByServicioFinalizada(idServicio)
+        return (reportes) ? reportes : []
+    } catch (error) {
+        throw error
+    }
+}
+
+const getAllAsignacionEsperaByCiudadServicioFinalizada = async (idCiudad, idServicio) => {
+    try {
+        const reportes = await reportesRepository.getAllAsignacionEsperaByCiudadServicioFinalizada(idCiudad, idServicio)
+        return (reportes) ? reportes : []
+    } catch (error) {
+        throw error
+    }
+}
+
 module.exports = {
     getAllAsignacionEspera,
     getAllAsignacionEsperaByCiudad,
     getAllAsignacionEsperaByServicio,
     getAllAsignacionEsperaByCiudadServicio,
+    getAllAsignacionFinalizado,
+    getAllAsignacionEsperaByCiudadFinalizada,
+    getAllAsignacionEsperaByServicioFinalizada,
+    getAllAsignacionEsperaByCiudadServicioFinalizada,
 }
